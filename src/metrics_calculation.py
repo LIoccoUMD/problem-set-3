2a6ae53446dd3356178b425c237658c6df635806
LGI4 <lucianosoccer4@gmail.com>
2024-08-13 17:26:44 -0400

    requirements.txt and metrics_calculation.py completed

--- src/metrics_calculation.py
+++ src/metrics_calculation.py
@@ -38,7 +38,32 @@ def calculate_metrics(model_pred_df, genre_list, genre_true_counts, genre_tp_cou
 
     '''
 
-    # Your code here
+    tp_sum = model_pred_df['correct?'].sum()
+    fp_sum = len(model_pred_df) - tp_sum
+    fn_sum = sum([genre_true_counts[genre] - genre_tp_counts[genre] for genre in genre_list])
+    
+    micro_precision = tp_sum / (tp_sum + fp_sum) if (tp_sum + fp_sum) > 0 else 0
+    micro_recall = tp_sum / (tp_sum + fn_sum) if (tp_sum + fn_sum) > 0 else 0
+    micro_f1 = (2 * micro_precision * micro_recall) / (micro_precision + micro_recall) if (micro_precision + micro_recall) > 0 else 0
+    
+    macro_prec_list = []
+    macro_recall_list = []
+    macro_f1_list = []
+
+    for genre in genre_list:
+        tp = genre_tp_counts[genre]
+        fp = genre_fp_counts[genre]
+        fn = genre_true_counts[genre] - tp
+
+        precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+        recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+        f1 = (2 * precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+        
+        macro_prec_list.append(precision)
+        macro_recall_list.append(recall)
+        macro_f1_list.append(f1)
+    
+    return micro_precision, micro_recall, micro_f1, macro_prec_list, macro_recall_list, macro_f1_list
 
     
 def calculate_sklearn_metrics(model_pred_df, genre_list):
@@ -61,4 +86,27 @@ def calculate_sklearn_metrics(model_pred_df, genre_list):
     true_matrix = pd.DataFrame(true_rows)
     '''
 
-    # Your code here
+    pred_rows = []
+    true_rows = []
+    
+    for index, row in model_pred_df.iterrows():
+        actual_genres = eval(row['actual genres'])
+        predicted_genre = row['predicted']
+
+        true_vector = [1 if genre in actual_genres else 0 for genre in genre_list]
+        pred_vector = [1 if genre == predicted_genre else 0 for genre in genre_list]
+        
+        true_rows.append(true_vector)
+        pred_rows.append(pred_vector)
+
+    true_matrix = pd.DataFrame(true_rows, columns=genre_list)
+    pred_matrix = pd.DataFrame(pred_rows, columns=genre_list)
+
+    macro_precision, macro_recall, macro_f1, _ = precision_recall_fscore_support(
+        true_matrix, pred_matrix, average='macro'
+    )
+    micro_precision, micro_recall, micro_f1, _ = precision_recall_fscore_support(
+        true_matrix, pred_matrix, average='micro'
+    )
+    
+    return macro_precision, macro_recall, macro_f1, micro_precision, micro_recall, micro_f1
